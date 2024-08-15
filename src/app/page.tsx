ae90aa4baee9309a43f459a0d5a5044b347e673f
Sandunika Nawullage <sandunikanawullage201@gmail.com>
2024-08-15 16:30:55 -0700

    Updated All Reservations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,8 @@
-'use client';
-import { Box, Grid } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import MiniDrawer from './components/admin-dashboard/side-nav/sidenav'; 
-import StatsRow from './components/admin-dashboard/stat-row/statrow';
-import Upcomingevents from './components/admin-dashboard/upcoming-events/upcomingevents';
+import Image from "next/image";
+import styles from "./page.module.css";
 
-
-
-
-const Page: React.FC = () => {
-
-  const [userId, setUserId] = useState<number>(0);
-
-  useEffect(() => {
-    const storedUser = localStorage.getItem('user');
-    if (storedUser) {
-      const parsedUser = JSON.parse(storedUser);
-      setUserId(parsedUser?.id);
-    }
-  },[userId]);
-
- 
-
+export default function Home() {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <MiniDrawer childTitle="Dashboard" />
-      <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: 8 }}>
-        <StatsRow organizationId={userId}/>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={8}>
-            <Upcomingevents/>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            
-          </Grid>
-        </Grid>
-      </Box>
-    </Box>
+    <h1>Home</h1>
   );
-};
-
-// export default organizationAuth(Page);
-export default Page;
+}
