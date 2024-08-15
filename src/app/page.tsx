150b1a88552ab78a36dd88430529080636b9eef2
Avindu21894 <a.payagalagegr52017@gmail.com>
2024-08-15 23:12:57 +0530

    homepage image carousel created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import ResponsiveAppBar from "./components/navbar/navbar";
 import NavBar from "./components/navbar/navbar";
+import ImageSlider from "./components/image-slider/image-slider";
 
 export default function Home() {
   return (
     <>
     <NavBar />
+    <ImageSlider />
     </>
   );
 }
