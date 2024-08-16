c0a6c473874c90ef0922f1dde5b4510b69f6262c
E.L.D.Chamika <chamikadilki9@gmail.com>
2024-08-16 23:45:20 +0530

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+'use client';
 import Image from "next/image";
 import styles from "./page.module.css";
+import Footer from "./components/footer/footer";
+
+
+
+
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <><h1>Home</h1>
+    <Footer/>
+    </>
   );
 }
